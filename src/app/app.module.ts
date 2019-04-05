47a697988fc49874b78af4cb9e8ccf2a5c63e4ea
TheFalseTruth <30677667+TheFalseTruth@users.noreply.github.com>
2019-04-05 18:11:10 +0700

    Update favicon.ico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainHeaderComponent } from './main-header/main-header.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainHeaderComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
