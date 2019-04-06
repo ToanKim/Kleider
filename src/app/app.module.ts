cdae996e1f699d91a624d48e82e4fe448684270e
TheFalseTruth <kimtoan1998@gmail.com>
2019-04-06 10:58:01 +0700

    Added new font, Leaflet map and Calendar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainHeaderComponent } from './main-header/main-header.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LeafletMapComponent } from './leaflet-map/leaflet-map.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainHeaderComponent,
-    NavBarComponent
+    NavBarComponent,
+    LeafletMapComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
