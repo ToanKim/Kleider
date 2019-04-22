e9c1eeb85c8da352bec90eec667d32a9608dbb2c
TheFalseTruth <30677667+TheFalseTruth@users.noreply.github.com>
2019-04-22 10:15:06 +0700

    Added About Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { RegisterComponent } from './register/register.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { ShippingComponent } from './shipping/shipping.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'cart', component: ShoppingCartComponent},
   { path: 'shipping', component: ShippingComponent},
-  { path: 'homepage', component: MainPageComponent}
+  { path: 'homepage', component: MainPageComponent},
+  { path: 'about', component: AboutPageComponent}
 ];
 
 @NgModule({
