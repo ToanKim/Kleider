0b385669c41a50c8a46a9571706cd051c38abf60
huymaxxx25 <49311569+huymaxxx25@users.noreply.github.com>
2019-05-14 19:47:09 +0700

    Take information of product from database
    Take information of product from database

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 
+//database
+import {AngularFireDatabase} from 'angularfire2/database';
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent  {
 
-  constructor() { }
+  Product: any[];
+ 
 
-  ngOnInit() {
+  constructor(db: AngularFireDatabase){
+
+    //product
+    db.list('/Vu-test/product1').valueChanges().subscribe(Product => {
+      this.Product = Product;
+      console.log(this.Product[0]);
+    })
+
+    db.list('/Vu-test').valueChanges().subscribe(Product => {
+      this.Product = Product;
+      console.log(this.Product[1]);
+    })
+
+    db.list('/Vu-test').valueChanges().subscribe(Product => {
+      this.Product = Product;
+      console.log(this.Product[2]);
+    })
+
+    db.list('/Vu-test').valueChanges().subscribe(Product => {
+      this.Product = Product;
+      console.log(this.Product[3]);
+    })
+
+    db.list('/Vu-test').valueChanges().subscribe(Product => {
+      this.Product = Product;
+      console.log(this.Product[4]);
+    })
+
+    db.list('/Vu-test').valueChanges().subscribe(Product => {
+      this.Product = Product;
+      console.log(this.Product[5]);
+    })
   }
-
 }
