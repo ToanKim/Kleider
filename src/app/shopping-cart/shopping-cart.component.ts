2da1ee32380cbf6a95c0a497528247552b99d837
huymaxxx25 <49311569+huymaxxx25@users.noreply.github.com>
2019-05-15 09:43:07 +0700

    fix err
    fix err

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { ProductDisplay } from '../Models/ProductDisplay.Model';
+
+import { ProductService } from '../Services/product.service';
+import { Product } from '../Models/Product.Model';
+import { IAlert } from '../Models/IAlert';
+import { SharedService } from '../Services/shared.service';
+import { min } from 'rxjs/operators';
 
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
   styleUrls: ['./shopping-cart.component.css']
 })
-export class ShoppingCartComponent implements OnInit {
 
+export class ShoppingCartComponent implements OnInit {
+  count = 1;
   constructor() { }
 
   ngOnInit() {
   }
+  
+  add(){
+    this.count = this.count + 1;
+  }
 
+  sub(){
+    if (this.count > 0 ) 
+    {
+      this.count = this.count - 1;
+    }
+    else
+    this.count = 0;
+    
+  }
 }
