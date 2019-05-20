ea0bc35b44d03bf963fc27b0fda0caed9c9c39d5
huymaxxx25 <49311569+huymaxxx25@users.noreply.github.com>
2019-05-20 21:23:43 +0700

    add cart service
    fix display data and add or sub product and price

---
+++ src/app/core/cart.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { CanActivate } from '@angular/router/src/utils/preactivation';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartGuard implements CanActivate {
+  path: ActivatedRouteSnapshot[];
+  route: ActivatedRouteSnapshot;
+  constructor(
+    public auth: AuthService,
+    public router: Router
+  ) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    const uid = JSON.parse(localStorage.getItem('user')).uid;
+
+    // console.log(uid);
+    if (JSON.parse(localStorage.getItem(`${uid}`)) == null) {
+      alert(`You don't have any products in your cart`);
+      this.router.navigate(['/homepage']);
+    }
+    return true;
+  }
+}
