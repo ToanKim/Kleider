40149a4aa9943c763ea63a95550b87ad0c54a1fa
huymaxxx25 <49311569+huymaxxx25@users.noreply.github.com>
2019-05-20 21:09:59 +0700

    add function to add product to cart store local
    add function to add product to cart store local

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,7 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
 //database
-import {AngularFireDatabase} from 'angularfire2/database';
+import {AngularFireDatabase, AngularFireList} from 'angularfire2/database';
+// import {FirebaseListObservable } from '@angular/fire'
+import { FirebaseApp } from '@angular/fire';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { realpath } from 'fs';
+import { AuthService } from '../core/auth.service';
+import { Router } from '@angular/router';
+import { Cart } from '../interface/cart';
+
+// import { auth } from 'firebase';
+
+
+// import {Cart} from "../Models/Cart.model";
+
+class CartList{
+  constructor (public PID){};
+}
+
 
 @Component({
   selector: 'app-main-page',
@@ -11,13 +31,55 @@ import {AngularFireDatabase} from 'angularfire2/database';
 export class MainPageComponent  {
 
   Product: any[];
- 
+  public cart: AngularFireList<CartList[]>;
 
-  constructor(db: AngularFireDatabase){
+  public router: Router;
+
+  private cartCount = 0;
+  // public auth: AuthService;
+  // private filter = ''; filter 
+
+  constructor(db: AngularFireDatabase,
+    public auth: AuthService   
+  ){
+
 
     //product
-    db.list('/Vu-test').valueChanges().subscribe(prd => {
-      this.Product = prd;
-    });
-  }
+    db.list('/Vu-test').valueChanges().subscribe(Product => {
+      this.Product = Product;
+      })
+
+    //  db.list('/user-cart').snapshotChanges().subscribe( cart => {
+    //    this.cart[0] = cart;
+    //  })  
+     };
+
+     AddtoCart(PID: number){
+       console.log(this.auth);
+       if (!this.auth.isLoggedIn)
+       {
+        this.router.navigate(['/customer'])
+       } else {
+         var productCart = new Array();
+         var currentCart = JSON.parse(localStorage.getItem(`${this.auth.userData.uid}`));
+         if (currentCart !== null) {
+           productCart = currentCart.productID
+         }
+
+         if (productCart.includes(PID)){
+           alert('You already have this product in cart')
+         } else {
+        //  console.log(productCart);
+         productCart.push(PID);
+         const product: Cart = {
+           productID: productCart
+         }
+         localStorage.setItem(`${this.auth.userData.uid}`, JSON.stringify(product));
+        }
+      }
+        // alert("Product " + productCart + " is added.");
+     }
 }
+    
+    
+    
